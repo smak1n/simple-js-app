56444c5077953de1b83e574f2ac85b4e7b82110d
Ismail <vinux181@gmail.com>
2021-10-08 17:21:03 +0200

    removing existing content
    removing old content in order to create a new project for the exercise 1.2 ( creating an array of 4 pokemons)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-alert('Hello World');
-
-let favoriteFood = 'Broccoli';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Balbasaur',
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: "Charizard",
+        height: 1.7,
+        type:['fire', 'flying']
+    },
+    {
+        name: "Torchic",
+        height: 0.4,
+        type: 'fire'
+    },
+    {
+        name: "Rhydon",
+        height: 1.9,
+        type: ['frock', 'ground']
+    }
+];
