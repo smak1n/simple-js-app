f28800310560a8b5e83e720d671474f16ba5813b
Ismail <vinux181@gmail.com>
2021-10-14 18:52:39 +0200

    changes in styles.css and scripts.js
    adding some stylings in the css file and
    adding changes to script.js to properly display list items

--- js/scripts.js
+++ js/scripts.js
@@ -20,16 +20,13 @@ let pokemonList = [
         type: ['frock', 'ground']
     }
 ];
-
+document.write(`<ul>`); //creating unordered list
 for (let i = 0; i < pokemonList.length; i++)  {
-    document.write(`<ul>`) //creating unordered list
-    document.write(`<li>${pokemonList[i].name} (Height: ${pokemonList[i].height})`); 
-    //creating list items for unordered list
-    // Writes pokemon names and heights on website's DOM
 
+    document.write(`<li> <p> ${pokemonList[i].name} (Height: ${pokemonList[i].height})`); //creating list items for unordered list and Writes pokemon names and heights on website's DOM 
     if (pokemonList[i].height > 1.9)  {
       document.write(' - Wow, that\'s big!'); //adding code to highlight big pokemons in the list
     }
-    document.write(`</li>`);
-    document.write(`</ul>`);
+    document.write(`</p> </li>`);
 }
+document.write(`</ul>`);
