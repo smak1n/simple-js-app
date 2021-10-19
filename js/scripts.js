4903dec3e0fa4aba647ef61fb72ce438a312f876
Ismail <vinux181@gmail.com>
2021-10-19 16:11:12 +0200

    Wrap PokemonList array in an IIFE
    Wrapping pokemonList in an IIFE to avoid accidentally accessing the global state;
    -----------------------
    adding a new pokemon to pokemonList array;
    ----------------------
    updating the loop code to cope with the new changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,46 @@
-let pokemonList = [
-    {
-        name: 'Balbasaur',
-        height: 0.7,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        type: ['fire', 'flying']
-    },
-    {
-        name: 'Slowking',
-        height: 2,
-        type: ['psychic', 'water']
-    },
-    {
-        name: 'Rhydon',
-        height: 1.9,
-        type: ['frock', 'ground']
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Balbasaur',
+            height: 0.7,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            type: ['fire', 'flying']
+        },
+        {
+            name: 'Slowking',
+            height: 2,
+            type: ['psychic', 'water']
+        },
+        {
+            name: 'Rhydon',
+            height: 1.9,
+            type: ['frock', 'ground']
+        }
+    ];
 
-//Using forEach() function instead of the 'for' loop function
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+//adding new pokemon to pokemonList array
+pokemonRepository.add({name: 'Natu', height:0.08 , type: ['Psychic', 'Flying']});
+console.log(pokemonRepository.getAll());
+
+//Using forEach() function instead of the 'for' loop function // Outside of the IIFE
 function myPokemonRepository(pokemon) {
     document.write('<p>' + pokemon.name + ' (Height:' + pokemon.height + ')');
       if (pokemon.height > 1.9) {
@@ -29,4 +48,5 @@ function myPokemonRepository(pokemon) {
       }
         document.write('</p>')
 }
-pokemonList.forEach(myPokemonRepository);
+pokemonRepository.getAll().forEach(myPokemonRepository);
+
