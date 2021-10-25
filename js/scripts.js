8d294a0c450d659b51458ab305872dc60cc30ea5
Ismail <vinux181@gmail.com>
2021-10-25 16:39:22 +0200

    fixing indentation scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,14 +29,14 @@ let pokemonRepository = (function () {
   function add(pokemon) {
     if (
       typeof pokemon === 'object' &&
-        'name' in pokemon &&
-        'height' in pokemon &&
-        'type' in pokemon
-      ) {
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'type' in pokemon
+    ) {
       pokemonList.push(pokemon);
-      } else {
+    } else {
         console.log('pokemon is not correct');
-      }
+    }
   }
 
   function addListItem(pokemon) {
