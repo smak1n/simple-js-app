1e235890b836640daafcab43c13a48a5f51fe5b8
Ismail <vinux181@gmail.com>
2021-10-30 17:18:19 +0200

    updating scripts.js
    adding a function to show and hide modal to overlay information about pokemons;
    adding EventListener to listen for clicking ESC button , clicking outside of the modal container so it can close it

--- js/scripts.js
+++ js/scripts.js
@@ -66,9 +66,60 @@ let pokemonRepository = (function () {
 
   function showDetails(pokemon) { //creating function to show more details about pokemon when clicked
     loadDetails(pokemon).then(function() {
-      console.log(pokemon);
+      showModal(pokemon);
     });
   }
+
+  //showModal function
+  let modalContainer = document.querySelector('#modal-container');
+
+  function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+
+    let heightElement = document.createElement('p');
+    heightElement.innerText = 'Height: ' + pokemon.height;
+
+    let imageElement = document.createElement('img');
+    imageElement.classList.add('pokeImage');
+    imageElement.src = pokemon.imageUrl;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  //By pressing ESC key from keyboard to close modal
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+  // Adding eventListener when clicked outside the modal container to close it
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
   return {
     getAll: getAll,
